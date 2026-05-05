980493c36debddbc02ccc08da812dbc17585a16e
MahdiAhmedMC <mahdimceyfan@gmail.com>
2026-05-05 14:52:10 +0100

    Update player.cpp

--- src/practice_fixes/player.cpp
+++ src/practice_fixes/player.cpp
@@ -168,7 +168,9 @@ PlayerData PlayerPracticeFixes::saveData(PlayerObject* player) {
     data.m_maybeReverseAcceleration = player->m_maybeReverseAcceleration;
     data.m_xVelocityRelated2 = player->m_xVelocityRelated2;
     data.m_isDashing = player->m_isDashing;
+#ifdef GEODE_IS_WINDOWS
     data.m_unk9e8 = player->m_unk9e8;
+#endif
     data.m_groundObjectMaterial = player->m_groundObjectMaterial;
     data.m_vehicleSize = player->m_vehicleSize;
     data.m_playerSpeed = player->m_playerSpeed;
@@ -423,7 +425,9 @@ void PlayerPracticeFixes::applyData(PlayerObject* player, PlayerData data, bool 
     player->m_maybeReverseAcceleration = data.m_maybeReverseAcceleration;
     player->m_xVelocityRelated2 = data.m_xVelocityRelated2;
     if (!isFakePlayer) player->m_isDashing = data.m_isDashing;
+#ifdef GEODE_IS_WINDOWS
     player->m_unk9e8 = data.m_unk9e8;
+#endif
     player->m_groundObjectMaterial = data.m_groundObjectMaterial;
     player->m_vehicleSize = data.m_vehicleSize;
     player->m_playerSpeed = data.m_playerSpeed;
